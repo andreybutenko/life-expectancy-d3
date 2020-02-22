5d9c684f9aed035c9b48ed16e63da8bfb483b5a6
Andrey Butenko <ghostmancer@gmail.com>
2020-02-21 18:53:55 -0800

    Complete assignment

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# life-expectancy-d3
+# Life Expectancy D3 Data Visualization
+
+Interactive scatterplot showing the relationship between the life expectancy and fertility of nations. When a point is hovered over, a lineplot tooltip appears showing that country's population over time.
+
+**[See it live here!](https://andreybutenko.github.io/life-expectancy-d3/)**
+
+## Affiliation
+
+This project was developed as an assignment for INFO 474: Interactive Information Visualization at the [University of Washington Information School](https://ischool.uw.edu) by [Andrey Butenko](https://andreybutenko.com), February 2020.
+
+## Assignment Description
+
+Using the gapminder.csvPreview the document data create a scatter plot of fertility (x axis) vs life expectancy (y axis). Include only data from the year 1980.  When a user hovers over a point on the scatter plot, in the tooltip show a line graph of time (x axis) vs population (y axis) for that country associated with the hovered over point.
+
+Tableau uses a complicated algorithm to determine what points get labels. You do not have to match the labels on the Tableau viz. However, you should add a label of the country name for every country that has a population of over 100 million people. Put this label 20px to the right of the center of the point.
+
+You don't have to include a filter by year or a legend showing population size.
+
+Upload your code to gh-pages (or any other hosting service) and submit a link to the published website. 
--- chart.js
+++ chart.js
@@ -1,64 +1,68 @@
-const HEIGHT = 600;
-const WIDTH = 800;
-const MARGIN = 100;
-const LEGEND_SIZE = 85;
+const config = {
+  displayPlot: {
+    height: 800,
+    width: 1000,
+    margin: 200,
 
-const TYPE_COLORS = {
-  'Bug': '#4E79A7',
-  'Dark': '#A0CBE8',
-  'Electric': '#F28E2B',
-  'Fairy': '#FFBE7D',
-  'Fighting': '#59A14F',
-  'Fire': '#8CD17D',
-  'Ghost': '#B6992D',
-  'Grass': '#499894',
-  'Ground': '#86BCB6',
-  'Ice': '#FABFD2',
-  'Normal': '#E15759',
-  'Poison': '#FF9D9A',
-  'Psychic': '#79706E',
-  'Steel': '#BAB0AC',
-  'Water': '#D37295'
-};
+    title: 'Fertility vs Life Expectancy (1980)',
+    xSource: 'fertility',
+    xDisplay: 'Fertility',
+    ySource: 'life_expectancy',
+    yDisplay: 'Life Expectancy',
+    rFunction: d => Math.max(Math.sqrt(d.population) / 2000, 4),
+    label: {
+      source: 'country',
+      thresholdValue: 100000000,
+      thresholdSource: 'population',
+      offsetX: 20,
+      offsetY: 5
+    }
+  },
+  tooltipPlot: {
+    height: 350,
+    width: 350,
+    margin: 80,
 
-const TITLE = 'Pokemon Stats Total vs Special Defence'
-const X_AXIS = 'Sp. Def';
-const Y_AXIS = 'Total';
-const COLOR_DIM = 'Type 1';
+    xSource: 'year',
+    xDisplay: 'Year',
+    ySource: 'population',
+    yDisplay: 'Population',
+  }
+}
 
-let svg, scatterGroup, tooltipLayer, tooltipBackground, tooltipTextName, tooltipTextType, tooltipTextType2;
+let svg, scatterGroup, tooltipLayer, tooltipBackground, tooltipSvg, tooltipGroup;
 let data;
 let firstRender = true;
 
-function setupSvg() {
+function setupDisplaySvg() {
   svg = d3.select('.display')
     .append('svg')
-    .attr('width', WIDTH + 3 * MARGIN + LEGEND_SIZE)
-    .attr('height', HEIGHT + 2 * MARGIN);
+    .attr('width', config.displayPlot.width + 2 * config.displayPlot.margin)
+    .attr('height', config.displayPlot.height + 2 * config.displayPlot.margin);
 
   scatterGroup = svg.append('g')
-    .attr('transform', `translate(${MARGIN}, ${MARGIN})`);
+    .attr('transform', `translate(${config.displayPlot.margin}, ${config.displayPlot.margin})`);
 
   svg.append('text')
-    .attr('x', MARGIN / 2)
-    .attr('y', (HEIGHT + 2 * MARGIN) / 2)
-    .attr('transform-origin', `${MARGIN / 2} ${(HEIGHT + 2 * MARGIN) / 2}`)
+    .attr('x', config.displayPlot.margin / 2)
+    .attr('y', (config.displayPlot.height + 2 * config.displayPlot.margin) / 2)
+    .attr('transform-origin', `${config.displayPlot.margin / 2} ${(config.displayPlot.height + 2 * config.displayPlot.margin) / 2}`)
     .attr('transform', 'rotate(-90)')
     .attr('text-anchor', 'middle')
-    .text(Y_AXIS);
+    .text(config.displayPlot.yDisplay);
 
   svg.append('text')
-    .attr('x', (WIDTH + 2 * MARGIN) / 2)
-    .attr('y', HEIGHT + 1.5 * MARGIN)
+    .attr('x', (config.displayPlot.width + 2 * config.displayPlot.margin) / 2)
+    .attr('y', config.displayPlot.height + 1.5 * config.displayPlot.margin)
     .attr('text-anchor', 'middle')
-    .text(X_AXIS);
+    .text(config.displayPlot.xDisplay);
 
   svg.append('text')
-    .attr('x', (WIDTH + 2 * MARGIN) / 2)
-    .attr('y', 0.5 * MARGIN)
+    .attr('x', (config.displayPlot.width + 2 * config.displayPlot.margin) / 2)
+    .attr('y', 0.5 * config.displayPlot.margin)
     .attr('text-anchor', 'middle')
     .style('font-size', '1.5em')
-    .text(TITLE);
+    .text(config.displayPlot.title);
 }
 
 function addTooltipLayer() {
@@ -66,138 +70,159 @@ function addTooltipLayer() {
     .style('display', 'none');
 
   tooltipBackground = tooltipLayer.append('rect')
-    .attr('width', 200)
-    .attr('height', 70)
+    .attr('width', config.tooltipPlot.width + 2 * config.tooltipPlot.margin)
+    .attr('height', config.tooltipPlot.height + 2 * config.tooltipPlot.margin)
     .attr('fill', '#ecf0f1')
     .attr('stroke', '#000000')
     .attr('stroke-width', 1)
     .attr('rx', 15)
     .attr('ry', 15)
 
-  tooltipTextName = tooltipLayer.append('text')
-    .attr('x', 8)
-    .attr('y', 20);
+  tooltipSvg = tooltipLayer.append('svg')
+    .attr('width', config.tooltipPlot.width + 2 * config.tooltipPlot.margin)
+    .attr('height', config.tooltipPlot.height + 2 * config.tooltipPlot.margin);
 
-  tooltipTextType = tooltipLayer.append('text')
-    .attr('x', 8)
-    .attr('y', 40);
+    tooltipSvg.append('text')
+    .attr('x', config.tooltipPlot.margin / 2)
+    .attr('y', (config.tooltipPlot.height + 2 * config.tooltipPlot.margin) / 2)
+    .attr('transform-origin', `${config.tooltipPlot.margin / 2} ${(config.tooltipPlot.height + 2 * config.tooltipPlot.margin) / 2}`)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text(config.tooltipPlot.yDisplay);
 
-  tooltipTextType2 = tooltipLayer.append('text')
-    .attr('x', 8)
-    .attr('y', 60);
+    tooltipSvg.append('text')
+    .attr('x', (config.tooltipPlot.width + 2 * config.tooltipPlot.margin) / 2)
+    .attr('y', config.tooltipPlot.height + 1.5 * config.tooltipPlot.margin)
+    .attr('text-anchor', 'middle')
+    .text(config.tooltipPlot.xDisplay);
+
+  tooltipGroup = tooltipSvg.append('g')
+    .attr('transform', `translate(${config.tooltipPlot.margin}, ${config.tooltipPlot.margin})`);
 }
 
 function drawLegend() {
-  const legendGroup = svg.append('g')
-    .attr('transform', `translate(${1.2 * MARGIN + WIDTH}, ${MARGIN})`);
+  legendGroup = svg.append('g')
+    .attr('transform', `translate(${1.2 * config.displayPlot.margin + config.displayPlot.width}, ${config.displayPlot.margin})`);
 
   legendGroup.append('text')
     .attr('x', 0)
     .attr('y', 0)
     .text('Legend');
-  
-  for(let i = 0; i < Object.keys(TYPE_COLORS).length; i++) {
-    const type = Object.keys(TYPE_COLORS)[i];
-
-    legendGroup.append('text')
-      .attr('x', 20 + 5)
-      .attr('y', (i + 1) * 25 + 5)
-      .text(type);
-
-    legendGroup.append('rect')
-      .attr('x', 0)
-      .attr('y', i * 25 + 13)
-      .attr('width', 20)
-      .attr('height', 20)
-      .attr('fill', TYPE_COLORS[type])
-  }
 }
 
 function getData() {
-  d3.csv('pokemon.csv', newData => {
+  d3.csv('gapminder.csv', newData => {
     data = newData;
     redraw();
   });
 }
 
 function redraw() {
-  const pokemonGen = document.getElementById('pokemon-gen').value;
-  const pokemonLegend = document.getElementById('pokemon-legend').value;
-  const mappedData = data
-    .map(d => pokemonGen === 'all' ? { ...d, show: true } : { ...d, show: d['Generation'] == pokemonGen })
-    .map(d => pokemonLegend === 'all' ? d : { ...d, show: d.show && d['Legendary'] == pokemonLegend });
-  
-  draw(mappedData);
+  draw(scatterGroup, 'displayPlot', data);
 }
 
-function draw(data) {
-  const displayData = data.filter(d => d.show);
+function draw(group, configName, data, line) {
+  const displayData = (configName == 'displayPlot' ? data.filter(d => d.year == 1980) : data)
+    .filter(
+      d => d[config[configName].xSource] != 'NA' && d[config[configName].ySource] != 'NA'
+    );
 
   // Draw x axis
   const x = d3
     .scaleLinear()
-    .domain(d3.extent(displayData, d => +d[X_AXIS]))
-    .range([0, WIDTH]);
-  
-  if(firstRender) {
-    scatterGroup.append('g')
+    .domain(d3.extent(displayData, d => +d[config[configName].xSource]))
+    .range([0, config[configName].width]);
+    
+  if(group.select('g.x-axis').empty()) {
+    group.append('g')
       .attr('class', 'x-axis')
-      .attr('transform', `translate(0, ${HEIGHT})`)
+      .attr('transform', `translate(0, ${config[configName].height})`)
       .call(d3.axisBottom().scale(x));
   }
   else {
-    d3.select('.x-axis')
-      .transition(d3.transition().duration(1000))
+    group.select('g.x-axis')
       .call(d3.axisBottom().scale(x));
   }
   
   // Draw y axis
   const y = d3.scaleLinear()
-    .domain(d3.extent(displayData, d => +d[Y_AXIS]))
-    .range([HEIGHT, 0]);
+    .domain(d3.extent(displayData, d => +d[config[configName].ySource]))
+    .range([config[configName].height, 0]);
 
-  if(firstRender) {
-    scatterGroup.append('g')
+  if(group.select('g.y-axis').empty()) {
+    group.append('g')
       .attr('class', 'y-axis')
       .call(d3.axisLeft(y));
   }
   else {
-    d3.select('.y-axis')
-      .transition(d3.transition().duration(1000))
+    group.select('g.y-axis')
       .call(d3.axisLeft(y));
   }
+  
 
-  const selection = scatterGroup.selectAll('.point').data(data);
+  if(!line) {
+    group.selectAll('.point')
+      .data(displayData)
+      .enter()
+      .append('circle')
+      .attr('class', 'point')
+      .attr('r',
+        !config[configName].rFunction ?
+          4 :
+          d => config[configName].rFunction(d))
+      .on('mouseover', () => tooltipLayer.style('display', null))
+      .on('mouseout', () => tooltipLayer.style('display', 'none'))
+      .on('mousemove', d => {
+        const tooltipWidth = config.tooltipPlot.width + 2 * config.tooltipPlot.margin;
+        const tooltipHeight = config.tooltipPlot.height + 2 * config.tooltipPlot.margin;
 
-  selection.enter()
-    .append('circle')
-    .attr('class', 'point')
-    .attr('r', 5)
-    .style('fill', d => TYPE_COLORS[d[COLOR_DIM]])
-    .on('mouseover', () => tooltipLayer.style('display', null))
-    .on('mouseout', () => tooltipLayer.style('display', 'none'))
-    .on('mousemove', d => {
-      tooltipLayer.attr('transform', `translate(${x(d[X_AXIS]) + 20}, ${y(d[Y_AXIS]) + 20})`).raise()
-      tooltipTextName.text(d['Name']);
-      tooltipTextType.text(`Type 1: ${d['Type 1']}`);
-      tooltipTextType2.text(`Type 2: ${d['Type 2']}`);
-    })
-    .merge(selection)
-    .transition()
-    .duration(1000)
-    .attr('cx', d => x(d[X_AXIS]))
-    .attr('cy', d => y(d[Y_AXIS]))
-    .style('opacity', d => +d.show)
+        let xPosition = x(d[config[configName].xSource]) + 20;
+        let yPosition = y(d[config[configName].ySource]) - tooltipHeight / 2;
+
+        if(xPosition + tooltipWidth > config.displayPlot.width) {
+          xPosition = x(d[config[configName].xSource]) - tooltipWidth - 20;
+        } 
+        if(yPosition + tooltipHeight / 2 > config.displayPlot.height - 100) {
+          yPosition = y(d[config[configName].ySource]) - tooltipHeight - 20;
+        }
+        
+        tooltipLayer.attr('transform', `translate(${xPosition}, ${yPosition})`).raise();
+        draw(tooltipGroup, 'tooltipPlot', data.filter(d2 => d2.country == d.country), true);
+      })
+      .attr('cx', d => x(d[config[configName].xSource]))
+      .attr('cy', d => y(d[config[configName].ySource]));
+  }
+  else {
+    group.selectAll('path').remove();
+    group.append('path')
+      .datum(displayData)
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('d', d3.line()
+        .x(function(d) { return x(d[config[configName].xSource]) })
+        .y(function(d) { return y(d[config[configName].ySource]) })
+      )
+  }
+  
+  if(!!config[configName].label) {
+    const labelData = displayData.filter(d => d[config[configName].label.thresholdSource] > config[configName].label.thresholdValue);
+
+    group.selectAll('.label')
+      .data(labelData)
+      .enter()
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', d => x(d[config[configName].xSource]) + config[configName].label.offsetX)
+      .attr('y', d => y(d[config[configName].ySource]) + config[configName].label.offsetY)
+      .text(d => d[config[configName].label.source]);
+  }
 
   firstRender = false;
 }
 
 window.addEventListener('load', () => {
-  setupSvg();
-  drawLegend();
+  setupDisplaySvg();
+  // drawLegend();
   addTooltipLayer();
   getData();
-
-  document.getElementById('pokemon-gen').addEventListener('change', redraw);
-  document.getElementById('pokemon-legend').addEventListener('change', redraw);
 }, false);
